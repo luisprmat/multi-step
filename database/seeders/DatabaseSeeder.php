1daf1362cea0abeabee889b790f863d882507981
Luis Parrado <luisprmat@gmail.com>
2024-12-11 22:39:05 -0500

    Define DB structure + test data (seeder)

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
+use App\Models\City;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
+use App\Models\Country;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,11 +15,24 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
+
+        Country::create(['name' => 'United States']);
+        Country::create(['name' => 'United Kingdom']);
+        Country::create(['name' => 'Germany']);
+        Country::create(['name' => 'Colombia']);
+
+        City::create(['country_id' => 1, 'name' => 'New York', 'adult_price' => 100, 'children_price' => 75]);
+        City::create(['country_id' => 1, 'name' => 'Washington', 'adult_price' => 150, 'children_price' => 100]);
+        City::create(['country_id' => 2, 'name' => 'London', 'adult_price' => 200, 'children_price' => 175]);
+        City::create(['country_id' => 2, 'name' => 'Birmingham', 'adult_price' => 250, 'children_price' => 195]);
+        City::create(['country_id' => 3, 'name' => 'Berlin', 'adult_price' => 125, 'children_price' => 85]);
+        City::create(['country_id' => 3, 'name' => 'Stuttgart', 'adult_price' => 190, 'children_price' => 155]);
+        City::create(['country_id' => 4, 'name' => 'Bogotá', 'adult_price' => 80, 'children_price' => 60]);
+        City::create(['country_id' => 4, 'name' => 'Medellín', 'adult_price' => 50, 'children_price' => 42]);
+        City::create(['country_id' => 4, 'name' => 'Cartagena', 'adult_price' => 170, 'children_price' => 150]);
     }
 }
